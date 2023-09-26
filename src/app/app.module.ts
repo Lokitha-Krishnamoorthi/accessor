3122d22c195ef80d0c9476f2ce308b8e71312832
lokitha-it19 <lokitha.it19@bitsathy.ac.in>
2023-09-27 05:28:40 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputfieldComponent } from './inputfield/inputfield.component';
+import {MatCardModule} from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputfieldComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/inputfield/inputfield.component.ts
@@ -0,0 +1,42 @@
+import { Component ,OnInit,Input, ChangeDetectionStrategy} from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-inputfield',
+  templateUrl: './inputfield.component.html',
+  styleUrls: ['./inputfield.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers:[
+    {
+      provide: NG_VALUE_ACCESSOR, // accessor token now angular will know this is a form component
+      useExisting: InputfieldComponent,
+      multi:true // we have multi field
+    }
+  ]
+})
+export class InputfieldComponent implements OnInit, ControlValueAccessor{
+
+  @Input() label: string="";
+  value!:string;
+  onChange!: (value: string) => void;
+  onTouched!: () => void;
+  constructor() {}
+
+  //write value from the form control instance to native html control
+  writeValue(obj: any): void { 
+    this.value=obj;
+  }
+
+  //propagate value native to form control
+  registerOnChange(fn: any): void { 
+    this.onChange=fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched =fn;
+  }
+  
+
+  ngOnInit(): void {
+    
+  }
+}
